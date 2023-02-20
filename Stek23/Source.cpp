cacb62ae631dc06c91048fdf99969928378642f6
Sergey Makletsov <smaklets@kpfu.ru>
2023-02-20 13:30:13 +0300

    создание шаблонных функций

--- Stek23/Source.cpp
+++ Stek23/Source.cpp
@@ -5,15 +5,15 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
-	elem* stack1 = nullptr;
-	int values[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	elem<char*>* stack1 = nullptr;
+	const char* values[] = { "один", "два", "три", "четыре", "пять", "6", "7", "8", "9", "10" };
 	for(int i = 0; i<10; i++)
 	{
 		cout << values[i] << " ";
-		push(stack1, values[i]);
+		push(stack1, (char*)values[i]);
 	}
 	cout << endl;
-	int val;
+	char* val;
 	while (pop(stack1, val))
 	{
 		cout << val << " ";
--- Stek23/stek.h
+++ Stek23/stek.h
@@ -1,11 +1,35 @@
 #pragma once
 
+template <typename T>
 struct elem
 {
-	int x;
+	T x;
 	elem* next = nullptr;
 };
 
-void push(elem*& stek, int value);
-bool pop(elem*& stek, int& value);
-bool peek(elem* stek, int& value);
+template <typename T>
+void push(elem<T>*& top, T value) {
+	elem<T>* new_el = new elem<T>;
+	new_el->x = value;
+	new_el->next = top;
+	top = new_el;
+}
+
+template <typename T>
+bool pop(elem<T>*& top, T& value) {
+	if (!top) return false;
+
+	value = top->x;
+	auto old_el = top;
+	top = top->next;
+	delete old_el;
+	return true;
+}
+
+template <typename T>
+bool peek(elem<T>* top, T& value) {
+	if (!top) return false;
+
+	value = top->x;
+	return true;
+}
